e84d462c489d5e56c7600285703ee80c58dcc63e
HermanFassett <hermanfassett@gmail.com>
2018-08-26 18:00:07 -0700

    Add file upload and python script run capability

--- controllers/api.js
+++ controllers/api.js
@@ -1,28 +1,29 @@
-const { promisify } = require('util');
 const request = require('request');
+const path = require('path');
+const fs = require('fs-extra');
 
 /**
- * GET /api
- * List of API examples.
+ * POST /api/convert
+ * File convert to PDF by python
  */
-exports.getApi = (req, res) => {
-  res.render('api/index', {
-    title: 'API'
+exports.postConvert = (req, res) => {
+  // Call python script on data
+  const { spawn } = require('child_process');
+  const scriptPath = path.posix.join(process.cwd(), 'kassia/pdfmaker/kassia.py');
+  const inputFile = path.posix.join(process.cwd(), 'uploads', req.user._id.toString(), req.file.filename);
+  const outputPath = path.posix.join(process.cwd(), 'output', req.user._id.toString());
+
+  fs.mkdirsSync(outputPath)
+
+  const pyScript = spawn('python', [scriptPath, inputFile, `${outputPath}/${req.file.filename}.pdf`]);
+
+  // Get data while python script is running
+  pyScript.stdout.on('data', function(data) {
+    console.log("Data:", data.toString());
   });
-};
 
-/**
- * GET /api/upload
- * File Upload API example.
- */
-
-exports.getFileUpload = (req, res) => {
-  res.render('api/upload', {
-    title: 'File Upload'
+  // Redirect on script completion
+  pyScript.stdout.on('end', function() {
+    res.redirect(`/output/${req.user._id.toString()}/${req.file.filename}`);
   });
-};
-
-exports.postFileUpload = (req, res) => {
-  req.flash('success', { msg: 'File was uploaded successfully.' });
-  res.redirect('/api/upload');
-};
+}
